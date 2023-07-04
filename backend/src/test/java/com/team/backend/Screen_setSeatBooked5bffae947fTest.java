1e6bc21b7ee2d580b5605fa9a7985234271c4ea3
akshatjain04 <akshatjain0410@gmail.com>
2023-07-04 11:49:45 +0000

    Test generated by RoostGPT for test cinema-website-test using gpt

---
+++ backend/src/test/java/com/team/backend/Screen_setSeatBooked5bffae947fTest.java
@@ -0,0 +1,17 @@
+<project xmlns="http://maven.apache.org/POM/4.0.0"
+  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+  xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
+  <modelVersion>4.0.0</modelVersion>
+
+  <groupId>com.team</groupId>
+  <artifactId>backend</artifactId>
+  <version>1.0</version>
+
+  <!-- Add your dependencies here -->
+  <dependencies>
+    <dependency>
+      <!-- dependency details -->
+    </dependency>
+    <!-- Other dependencies -->
+  </dependencies>
+</project>
